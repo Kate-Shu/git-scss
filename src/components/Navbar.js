ed80cb76ece86de9e17ebf548d41f8017b847318
Kate-Shu <k.onufrichuk@gmail.com>
2022-10-16 14:23:19 +0200

    init comit

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../Styles/Navbar.css';
+// import '../Styles/generic.css';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="/home" className="nav">
+        Home
+      </a>
+
+      <p>Paragraph 1</p>
+
+      <div className="block">
+        <div className="block__text block__big">
+          Here is <span>span</span>
+          <p>Paragraph 2</p>
+        </div>
+        <a href="/home" className="nav">
+          Contact
+        </a>
+      </div>
+      <p className="mixin">mixin is here</p>
+      <h2>for git</h2>
+    </div>
+  );
+}
